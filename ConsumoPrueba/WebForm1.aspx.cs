0e26c72700c60f6b62018672d34748af270ae03c
Camilo Gonzalez V <camilo.igv@gmail.com>
2023-06-26 13:41:41 -0400

    consumo

---
+++ ConsumoPrueba/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumoPrueba
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ReferenciaAutosWS.WebService1SoapClient ws = new ReferenciaAutosWS.WebService1SoapClient();
+
+            DataTable dt = ws.listarAutos();
+            gv_datos.DataSource = dt;
+            gv_datos.DataBind();
+
+            List<ReferenciaAutosWS.AutoDTO> auto = ws.listarAutosObjeto().ToList();
+
+            gv_datos2.DataSource = auto;
+            gv_datos2.DataBind();
+
+        }
+    }
+}
