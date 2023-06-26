37405216ca4b2643c1c07e0c9190019db7aaec3a
Camilo Gonzalez V <camilo.igv@gmail.com>
2023-06-26 11:57:43 -0400

    init

---
+++ PruebaWS/App_Code/DatosDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Web;
+
+namespace datos
+{
+    public class DatosDAO
+    {
+        private static string string_conn = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=" + HttpContext.Current.Server.MapPath(@"App_Data\AUTOS.mdb") + ";";
+
+        internal static DataTable listarAutos()
+        {
+            string conn = string_conn;
+
+            string sql = "SELECT * FROM AUTO";
+            OleDbConnection cnn = new OleDbConnection(conn);
+            OleDbDataAdapter da = new OleDbDataAdapter(sql, cnn);
+            DataTable datos = new DataTable();
+
+            cnn.Open();
+            da.Fill(datos);
+
+            datos.TableName = "dt";
+            return datos;
+        }
+    }
+}
